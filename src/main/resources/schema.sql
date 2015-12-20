a8058036358482f4a09bcf6b9027908bf7f76d7f
BinodSethi <binod.sethi@reverside.co.za>
2015-12-20 20:05:40 +0200

    User Registration

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,29 @@
+/* table : user */
 create table users (
     id integer auto_increment not null primary key,
     first_name varchar(128) not null,
     last_name varchar(128) not null,
-    email varchar(32) not null, 
+    email varchar(32) not null,
+    phone_number varchar(16) not null,
     password varchar(32) not null, 
-    role varchar(16) not null,
     enabled boolean not null 
+);
+
+
+/* table : login */
+create table login (
+    id integer auto_increment not null primary key,
+    user_id integer not null,
+    user_name varchar(64) not null, 
+    password varchar(32) not null,
+    role varchar(16) not null,
+    constraint login_fk1 foreign key (user_id) references users(id)
+);
+
+/* table : loanTypes */
+create table loan_types (
+    id integer auto_increment not null primary key, 
+    name varchar(64) not null, 
+    description varchar(512),
+    image varchar(128)
 );
